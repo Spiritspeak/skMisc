7f292aec911ddf0fd4059f16872694a6ed628181
Spiritspeak <35294148+Spiritspeak@users.noreply.github.com>
2025-02-26 14:26:52 +0100

    moved ClassMetrics to aggregators

--- R/faststats.R
+++ R/faststats.R
@@ -258,54 +258,6 @@ twocor <- function(x, y, method="pearson"){
   return(invisible(test))
 }
 
-#' Get classification accuracy metrics
-#'
-#' @param x The true class memberships
-#' @param y The predicted class memberships
-#'
-#' @md
-#' @author Sercan Kahveci
-#' @return A vector with the following accuracy metrics:
-#' * Accuracy: \code{acc}
-#' * Chance accuracy: \code{chance_acc}
-#' * Cohen's Kappa: \code{kappa}
-#' * Positive predictive value: \code{ppv}
-#' * Chance positive predictive value: \code{chance_ppv}
-#' * Kappa of the positive predictive value: \code{kappa_ppv}
-#' * Negative predictive value: \code{npv}
-#' * Chance negative predictive value: \code{chance_npv}
-#' * Kappa of the negative predictive value: \code{kappa_npv}
-#' * Sensitivity: \code{sens}
-#' * Specificity: \code{spec}
-#' 
-#' @export
-#'
-#' @examples
-#' mymod <- glm(am ~ cyl + disp + hp,
-#'              family="binomial", 
-#'              data = mtcars)
-#' 
-#' newpred <- as.numeric(predict(mymod, 
-#'                               data=mtcars,
-#'                               type="response") > 0.5)
-#' 
-#' ClassMetrics(x=mtcars$am, y=newpred)
-#' 
-ClassMetrics <- function(x, y){
-  cm <- table(x, y)
-  return(c(acc=acc <- sum(diag(cm)) / sum(cm),
-           chance_acc=chance_acc <- max(rowSums(cm)) / sum(cm),
-           kappa=(acc-chance_acc) / (1 - chance_acc),
-           ppv=ppv <- cm[2,2] / sum(cm[,2]),
-           chance_ppv=chance_ppv <- sum(cm[2,]) / sum(cm),
-           kappa_ppv=(ppv - chance_ppv) / (1 - chance_ppv),
-           npv=npv <- cm[1,1] / sum(cm[,1]),
-           chance_npv=chance_npv <- sum(cm[1,]) / sum(cm),
-           kappa_npv=(npv - chance_npv) / (1 - chance_npv),
-           sens=cm[2,2] / sum(cm[2,]),
-           spec=cm[1,1] / sum(cm[1,]) ))
-}
-
 #' Multiple correlation
 #' Computes the \href{https://en.wikipedia.org/wiki/Multiple_correlation}{multiple correlation coefficient}
 #' of variables in \code{ymat} with the variable \code{x}
