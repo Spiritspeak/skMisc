47c98281d2b2d50e53d8bddba5b09d74b8d89041
Spiritspeak <35294148+Spiritspeak@users.noreply.github.com>
2025-08-27 18:17:44 +0200

    Added false discovery rate, revamped Bonferroni-Holm into p-value adjuster

--- R/faststats.R
+++ R/faststats.R
@@ -296,35 +296,63 @@ multiple.cor <- function(x, ymat, use="everything"){
 }
 
 
-#' Bonferroni-Holm significance evaluation
-#' Evaluate the significance of a vector of p-values according to
-#' the Bonferroni-Holm method
+#' Adjusted p-values for false-discovery rates and familywise error rates
 #' 
 #' @param x A vector of p-values.
-#' @param alpha An experiment-wise alpha-level to test against.
 #'
-#' @return A vector indicating whether values in \code{x} were significant
-#' according to the Bonferroni-Holm method.
+#' @return A vector of p-values adjusted in accordance with the chosen method.
 #' @author Sercan Kahveci
 #' @export
 #'
 #' @examples
-#' bonferroniHolm(c(1,.05,.001,.002,.01))
+#' oldp<-c(0.01,0.04,0.02,.1,0.035)
+#' bonferroniHolm(oldp)
+#' benjaminiHochberg(oldp)
+#' benjaminiYekutieli(oldp)
 #' 
-bonferroniHolm <- function(x, alpha=.05){
-  xord <- rank(x)
-  xlen <- length(x)
-  sigvec <- logical(xlen)
-  for(i in sort(unique(xord)) ){
-    idx <- which(xord == i)
-    if(x[idx[1]] < alpha/(xlen-sum(sigvec)) ){
-      sigvec[idx] <- T
-    }else{
-      break
-    }
-  }
-  return(sigvec)
+bonferroniHolm <- function(x){
+  xn <- x[!is.na(x)]
+  xord <- order(xn)
+  xlen <- length(xn)
+  newp <- xn[xord] * (xlen-seq_len(xlen)+1)
+  newp[newp>1] <- 1
+  # Bonferroni Holm monotonicity correction (up-propagated max)
+  out <- sapply(seq_len(xlen),function(y)max(newp[1:y]))[order(xord)]
+  x[!is.na(x)]<-out
+  return(x)
 }
+
+#' @rdname bonferroniHolm
+#' @export
+#' 
+benjaminiHochberg <- function(x){
+  xn <- x[!is.na(x)]
+  xord <- order(xn)
+  xlen <- length(xn)
+  newp <- xn[xord] * xlen/seq_len(xlen)
+  newp[newp>1] <- 1
+  # Benjamini Hochberg monotonicity correction (down-propagated min)
+  out <- sapply(seq_len(xlen),function(y)min(newp[y:xlen]))[order(xord)]
+  x[!is.na(x)]<-out
+  return(x)
+}
+
+#' @rdname bonferroniHolm
+#' @export
+#' 
+benjaminiYekutieli <- function(x){
+  xn <- x[!is.na(x)]
+  xord <- rank(xn)
+  xlen <- length(xn)
+  hf <- sum(1/(1:xlen))
+  newp <- xn * hf * xlen/(xord)
+  newp[newp>1] <- 1
+  # Benjamini Yekutieli monotonicity correction (down-propagated min)
+  out <- sapply(seq_len(xlen),function(y)min(newp[y:xlen]))[order(xord)]
+  x[!is.na(x)]<-out
+  return(x)
+}
+
 
 #' Generate a correlation table
 #' 
