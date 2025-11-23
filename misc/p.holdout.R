82a0320849fce6f3f917d5c861e8e3f4969d9b27
Spiritspeak <35294148+Spiritspeak@users.noreply.github.com>
2025-11-23 16:34:40 +0100

    planning holdout p-value

--- misc/p.holdout.R
+++ misc/p.holdout.R
@@ -7,18 +7,19 @@ thy<-rnorm(100)+xval
 # Outlier-driven correlation
 thx <- rnorm(100)
 thy <- rnorm(100)
-thx[1:2] <- thy[1:2] <- 50
+thx[1:2] <- thy[1:2] <- 10
 
-# Flip sign
 origh<-cor.holdout(x=thx,y=thy,goal="flip")
 
 hhh<-permute.cor.holdout(x=thx,y=thy,goal="flip",iters=10000)
 hist(hhh)
-mean(hhh > origh$h)
+mean(hhh >= origh$h)
 
 hhh2<-norm.cor.holdout(r=cor(thx,thy),n=length(thx),goal="flip",iters=1000)
 hist(hhh2,breaks=length(unique(hhh2)))
-mean(hhh2 < origh$h)
+mean(hhh2 <= origh$h)
+
+# Consider mid-p adjustment for p-values, as well as adding 1 to both numerator and denominator
 
 norm.cor.holdout<- function(r, n, iters=1000,
                             goal=c("nsig","flip"),
