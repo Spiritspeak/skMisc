03b87b99629021613ffde9e82ac99f8a3a27ad78
Spiritspeak <35294148+Spiritspeak@users.noreply.github.com>
2026-02-25 17:57:49 +0100

    added asinh, used ks test

--- R/transforms.R
+++ R/transforms.R
@@ -3,24 +3,34 @@
 
 #' Vector transformations
 #' 
-#' Normalizing transformations for skewed variables
+#' Normalizing transformations for skewed and leptokurtic variables
 #'
 #' @param x Vector to be transformed.
 #' @param type Can be "none" (no transform), "log" (\code{log(x)}), "log1p" (\code{log(x+1)}), 
-#' "sqrt" (\code{sqrt(x)}), "inv" (\code{1/x}), 
+#' "sqrt" (\code{sqrt(x)}), "inv" (\code{1/x}), "atan" (\code{atan(x)}),
+#' "asinh" (\code{asinh(x-mean(x)) + mean(x)}),
 #' "logshift" (\code{log(x+shift)-log(shift)}),
 #' "invshift" (\code{(1+shift)/(x+1+shift)}),
+#' "asinhrate" (\code{asinh((x-mean(x)) * rate) / rate + mean(x)}),
 #' "bcp" (Box-Cox transform from [car::bcPower()]), 
 #' "bcnp" (Shifted Box-Cox transform from [car::bcnPower()]),
 #' or "yj" (Yeo-Johnson transformation from [car::yjPower()]). Non-positive values in \code{x} are
-#' only allowed in "none", "bcnp" and "yj".
+#' only allowed in "none", "asinh", "asinhrate", "bcnp" and "yj".
 #' @param par A parameter that determines the shape of the transformation. If no parameter is given,
 #' the function finds the parameter that gives the most normally distributed values.
 #' For \code{"logshift"} and \code{"invshift"}, this is the shift; 
+#' For \code{"asinhrate"}, this is the rate, with higher values representing a stronger reduction of kurtosis;
 #' for \code{"bcp"} and \code{"yj"}, this is the lambda parameter; 
 #' for \code{"bcnp"}, this is two values: the lambda and gamma parameters.
 #' For all other transformations, \code{par} is ignored.
-#'
+#' 
+#' @details
+#' * For skewed distributions, consider "log", "sqrt", "inv", "atan", "logshift", "invshift", "bcp","bcnp", or "yjp".
+#' 
+#' * For distributions with excess kurtosis, consider "asinh" or "asinhrate".
+#' 
+#' @md
+#' @author Sercan Kahveci
 #' @returns A transformed vector. 
 #' Attribute \code{"transform.method"} indicates the used method, and 
 #' \code{"transform.par"} indicates the transformation parameter(s).
@@ -37,12 +47,14 @@
 #' bctrans<-trans(u,"bcp")
 #' shapiro.test(bctrans) # Normal
 #'
-#' u<-rt(100,2) 
+#' u<-rt(1000,2)+10 
 #' yjtrans<-trans(u,"yjp")
 #' shapiro.test(yjtrans) # Not normal; still leptokurtic
+#' asinhtrans<-trans(u,"asinh")
+#' shapiro.test(asinhtrans) # Normal!
 #' 
-#' 
-trans <- function(x,type=c("none","log","log1p","sqrt","inv","logshift","invshift","bcp","bcnp","yjp"),par=NULL){
+trans <- function(x,type=c("none","log","log1p","sqrt","inv","atan","asinh",
+                           "logshift","invshift","asinhrate","bcp","bcnp","yjp"),par=NULL){
   type<-match.arg(type)
   if(type=="none"){
     out<-x
@@ -54,13 +66,18 @@ trans <- function(x,type=c("none","log","log1p","sqrt","inv","logshift","invshif
     out<-sqrt(x)
   }else if(type=="inv"){
     out<-1/x
+  }else if(type=="atan"){
+    out<-atan(x)
+  }else if(type=="asinh"){
+    out<-asinh(x-mean(x))+mean(x)
   }else if(type=="logshift"){
     logshift<-function(x,p){
       log(x+p)-if(p==0){0}else{log(p)}
     }
     if(is.null(par)){
       scorer<-function(p){
-        -shapiro.test(logshift(x,exp(p)))$statistic
+        trval<-logshift(x,exp(p))
+        ks.test(trval,"pnorm",mean=mean(trval),sd=sd(trval))$statistic
       }
       par<-exp(optim(par=0,fn=scorer,method="BFGS")$par)
     }
@@ -71,11 +88,24 @@ trans <- function(x,type=c("none","log","log1p","sqrt","inv","logshift","invshif
     }
     if(is.null(par)){
       scorer<-function(p){
-        -shapiro.test(invshift(x,exp(p)))$statistic
+        trval<-invshift(x,exp(p))
+        ks.test(trval,"pnorm",mean=mean(trval),sd=sd(trval))$statistic
       }
       par<-exp(optim(par=0,fn=scorer,method="BFGS")$par)
     }
     out<-invshift(x,par[1])
+  }else if(type=="asinhrate"){
+    asinhrate<-function(x,p){
+      asinh((x-mean(x))*p)/p+mean(x)
+    }
+    if(is.null(par)){
+      scorer<-function(p){
+        trval<-asinhrate(x,exp(p))
+        ks.test(trval,"pnorm",mean=mean(trval),sd=sd(trval))$statistic
+      }
+      par<-exp(optim(par=0,fn=scorer,method="BFGS")$par)
+    }
+    out<-asinhrate(x,par[1])
   }else if(type=="bcp"){
     if(is.null(par)){
       par<-car::powerTransform(x,family="bcPower")$lambda
@@ -97,5 +127,6 @@ trans <- function(x,type=c("none","log","log1p","sqrt","inv","logshift","invshif
   attr(out,"transform.par")<-par
   return(out)
 }
+
 
 
